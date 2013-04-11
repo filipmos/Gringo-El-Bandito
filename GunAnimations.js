0244ef6d002bafd5e55a311dff7ea0f61c7c852d
filipmos <filip.mostafa@gmail.com>
2013-04-12 00:05:28 +0300

    Gun Animations
    Adding shooting and reloading animation

---
+++ GunAnimations.js
@@ -0,0 +1,15 @@
+var Fire : String;
+var Reload : String;
+
+function Start () {
+
+}
+
+function FireAnim () {
+	gameObject.animation.Play(Fire);
+}
+
+function ReloadAnim(){
+	gameObject.animation[Reload].speed = (gameObject.animation[Reload].clip.length / Gun.ReloadTTime);
+	gameObject.animation.Play(Reload);
+}
